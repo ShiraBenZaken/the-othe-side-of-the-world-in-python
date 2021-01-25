a58f5d32c66b1c0a890f1ac840d48d6489e16b39
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2021-01-25 12:25:51 +0200

    add a nice loop

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ from folium import Map, Marker, Popup
 from geo import GeoPoint
 
 # Get input values
-latitude = 31.78
-longitude = 35.04
+locations = [[31.78, 35.04],[41, -1],[36, 17]]
+
 # Foliom Map
-mymap = Map(location = [latitude, longitude])
-#create a GeoPoint
-geopoint = GeoPoint(latitude = latitude, longitude = longitude)
+mymap = Map(location = [31.78, 35.04])
 
-forecast = geopoint.get_weather()
-popup_content = f""" 
-{forecast[0][0][-8:-6]}h: {round(forecast[0][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width=35>
-<hr style="margin:1px">
-{forecast[1][0][-8:-6]}h: {round(forecast[1][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width=35>
-<hr style="margin:1px">
-{forecast[2][0][-8:-6]}h: {round(forecast[2][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width=35>
-<hr style="margin:1px">
-{forecast[3][0][-8:-6]}h: {round(forecast[3][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width=35>
-"""
+for lat, lon in locations: 
+    # Create a GeoPoint
+    geopoint = GeoPoint(latitude = lat, longitude = lon)
 
-popup = Popup(popup_content, max_width=400)
-popup.add_to(geopoint)
-geopoint.add_to(mymap)
+    forecast = geopoint.get_weather()
+    popup_content = f""" 
+    {forecast[0][0][-8:-6]}h: {round(forecast[0][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width=35>
+    <hr style="margin:1px">
+    {forecast[1][0][-8:-6]}h: {round(forecast[1][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width=35>
+    <hr style="margin:1px">
+    {forecast[2][0][-8:-6]}h: {round(forecast[2][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width=35>
+    <hr style="margin:1px">
+    {forecast[3][0][-8:-6]}h: {round(forecast[3][1])}*f <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width=35>
+    """
+
+    popup = Popup(popup_content, max_width=400)
+    popup.add_to(geopoint)
+    geopoint.add_to(mymap)
 
 # Save the map into a html file
 mymap.save("antipode.html")
